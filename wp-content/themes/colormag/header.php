749b29221b2805c629fd1aeba32d363e0092f9c4
vbak <vbak44@hotmail.com>
2017-04-29 16:48:25 +0300

    second commit

--- wp-content/themes/colormag/header.php
+++ wp-content/themes/colormag/header.php
@@ -45,7 +45,7 @@ wp_head();
             </div>
          <?php endif; ?>
 
-			<?php if (get_theme_mod('colormag_header_image_position', 'position_two') == 'position_one') { colormag_render_header_image(); } ?>
+
 
 			<div class="inner-wrap">
 
@@ -54,6 +54,7 @@ wp_head();
 						<?php
 						if((get_theme_mod('colormag_header_logo_placement', 'header_text_only') == 'show_both' || get_theme_mod('colormag_header_logo_placement', 'header_text_only') == 'header_logo_only')) {
 						?>
+							<?php if (get_theme_mod('colormag_header_image_position', 'position_two') == 'position_one') { colormag_render_header_image(); } ?>
 							<div id="header-logo-image">
 								<?php if (get_theme_mod('colormag_logo', '') != '') { ?>
 									<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo esc_url(get_theme_mod('colormag_logo')); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"></a>
